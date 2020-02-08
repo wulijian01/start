c035823f139e881f22df93cb73becf859732a2ca
wulijian01 <959813288@qq.com>
2020-02-08 11:16:46 +0800

    init

---
+++ src/main/java/com/dand/start/controller/HealthController.java
@@ -0,0 +1,15 @@
+package com.dand.start.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HealthController {
+
+    @GetMapping("/health")
+    @ResponseBody
+    public String health(){
+        return "success!!";
+    }
+}
