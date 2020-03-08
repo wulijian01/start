a691e2fa680b8bff1efc70f9c2395066832cb5a0
wulijian01 <959813288@qq.com>
2020-03-08 16:26:21 +0800

    init

---
+++ src/main/java/com/dand/start/User.java
@@ -0,0 +1,13 @@
+package com.dand.start;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User {
+
+    private String username;
+
+    private String password;
+}
--- src/main/java/com/dand/start/controller/HealthController.java
+++ src/main/java/com/dand/start/controller/HealthController.java
@@ -1,15 +1,23 @@
 package com.dand.start.controller;
 
+import com.dand.start.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HealthController {
 
-    @GetMapping("/health")
+    @PostMapping("/login2")
     @ResponseBody
-    public String health(){
+    public String health(User user){
+        System.out.println(user.getUsername());
         return "success!!";
+    }
+
+    @GetMapping("/")
+    public String login(){
+        return "login";
     }
 }
